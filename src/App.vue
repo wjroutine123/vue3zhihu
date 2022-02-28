a4fcc535921353aed0cdf232a2751ad9bfa2fb37
unknown <906957674@qq.com>
2022-02-28 21:17:33 +0800

    src

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div>{{ msg }}的年龄{{ age }}</div>
+  <button @click="add">+</button>
+  <div>{{ double }}</div>
+  <!-- 组件 -->
+  <content-child></content-child>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from "vue";
+import contentChild from "./components/contentChild.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+// 组件
   components: {
-    HelloWorld
-  }
-}
+    contentChild,
+  },
+  setup() {
+    const msg = ref("王大盒");
+    const age = ref(18);
+// 计算属性
+    const double = computed(() => {
+      return age.value * 2;
+    });
+// 点击事件
+    function add() {
+      age.value += 1;
+    }
+    return { msg, age, add,double };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/contentChild.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'contentChild',
+};
+</script>
+
+<style>
+</style>
